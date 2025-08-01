c868209a7619e318be7b84f6a48383a9f7853d85
Athar Imam <atahar.emam@nagarro.com>
2025-08-01 10:58:28 +0530

    removed extra files

--- YamlFiles/stopall.sh
+++
@@ -1,8 +0,0 @@
-kubectl delete -f emplapi-hpa.yaml
-kubectl delete -f emplapi-ing.yaml
-kubectl delete -f emplapi-depl-svc.yaml
-kubectl delete -f empldb-seed-job.yaml
-kubectl delete -f empldb-ssets-svc.yaml
-kubectl delete -f empldb-pv-pvc.yaml
-kubectl delete -f empldb-secret-config.yaml
-rm *.*
